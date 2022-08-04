1a861d7bd4bc43b17a452c72fa58a04ecda84144
KofTD <KOfTD@yandex.ru>
2022-08-04 15:26:37 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <filesystem>
+#include <iostream>
+#include <vector>
+namespace fs = std::filesystem;
+
+int main(int argc, char const *argv[])
+{
+    fs::path path_to_watch;
+    if (argc == 1)
+    {
+        path_to_watch = fs::current_path();
+    }
+    else
+    {
+        path_to_watch = argv[1];
+    }
+
+    std::vector<fs::path> files_in_dir;
+    for (auto &i : fs::directory_iterator(path_to_watch))
+        files_in_dir.push_back(i);
+
+    short j = 0;
+    for (auto &i : files_in_dir)
+    {
+        std::cout << i.filename().string() << "  ";
+        j++;
+        if (j == 4)
+        {
+            std::cout << std::endl;
+            j = 0;
+        }
+    }
+
+    return 0;
+}
